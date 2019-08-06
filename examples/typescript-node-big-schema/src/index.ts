42448a2566982802cdf5fec10e6f0e5d372858e1
aexol <aexol@aexol.com>
2019-08-06 21:26:00 +0200

    docs(added): playground
    Added url to playground with generated zeus code

--- examples/typescript-node-big-schema/src/index.ts
+++ examples/typescript-node-big-schema/src/index.ts
@@ -2,18 +2,18 @@ import { Chain } from "./graphql-zeus";
 const run = async () => {
   const chain = Chain("https://project-api.graphqleditor.com/graphql");
   const experimentalQuery = await chain.Query({
-    __alias: {
-      ccc: {
-        listProjects: [
-          { limit: 10 },
-          {
-            projects: {
+    listProjects: [
+      { owned: true },
+      {
+        projects: {
+          __alias: {
+            aaa: {
               name: true,
             },
           },
-        ],
+        },
       },
-    },
+    ],
   });
   console.log(experimentalQuery.ccc.listProjects!);
 };
