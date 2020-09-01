d03918cd09bc44b2e547c99ef83888ea7078abb6
aexol <aexol@aexol.com>
2020-09-01 14:02:56 +0200

    corrected git config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleNameMapper: {
+    '@/(.*)': ['<rootDir>/src/$1'],
+  },
   testMatch: ['**/__tests__/**/*.spec.(ts|tsx)'],
   watchPathIgnorePatterns: ['node_modules'],
   watchman: false,
