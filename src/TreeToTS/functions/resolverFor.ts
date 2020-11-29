7a2ed2328265ab02e1d36382ba7cdee5c038a96e
aexol <aexol@aexol.com>
2020-11-29 23:25:12 +0100

    resolver for

--- src/TreeToTS/functions/resolverFor.ts
+++ src/TreeToTS/functions/resolverFor.ts
@@ -7,9 +7,9 @@ export const resolverFor = <
   T extends keyof ValueTypes,
   Z extends keyof ValueTypes[T],
   Y extends (
-    args: Required<ValueTypes[T]>[Z] extends [infer Input, any] ? Input : never,
+    args: Required<ValueTypes[T]>[Z] extends [infer Input, any] ? Input : any,
     source: any,
-  ) => Z extends keyof ${MODEL_TYPES}[T] ? ${MODEL_TYPES}[T][Z] | Promise<${MODEL_TYPES}[T][Z]> : never
+  ) => Z extends keyof ${MODEL_TYPES}[T] ? ${MODEL_TYPES}[T][Z] | Promise<${MODEL_TYPES}[T][Z]> : any
 >(
   type: T,
   field: Z,
