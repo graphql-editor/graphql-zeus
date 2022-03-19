3aa2175ede4279e0407509cc1ca582260d7d8e62
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2022-03-19 19:00:50 +0000

    tests

--- src/plugins/typed-document-node/index.spec.ts
+++ src/plugins/typed-document-node/index.spec.ts
@@ -21,6 +21,31 @@ schema{
 `;
     const tree = Parser.parse(schema);
     const tdnResult = pluginTypedDocumentNode({ tree });
-    expect(tdnResult.ts).toContain(`TODO`);
+    expect(tdnResult.ts).toContain(`
+import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import gql from 'graphql-tag';
+import { GraphQLTypes, InputType, ValueTypes, Zeus } from './index';
+`);
+
+    expect(tdnResult.ts).toContain(`export function tq<Z extends ValueTypes["Query"]>(
+  query: Z | ValueTypes["Query"],
+):  TypedDocumentNode<InputType<GraphQLTypes["Query"], Z>, {}> {
+  return gql(Zeus("query", query));
+}`);
+
+    expect(tdnResult.ts).toContain(`
+export function tm<Z extends ValueTypes["Mutation"]>(
+  mutation: Z | ValueTypes["Mutation"],
+):  TypedDocumentNode<InputType<GraphQLTypes["Mutation"], Z>, {}> {
+  return gql(Zeus("mutation", mutation));
+}
+`);
+    expect(tdnResult.ts).toContain(`
+export function ts<Z extends ValueTypes["Subscription"]>(
+  subscription: Z | ValueTypes["Subscription"],
+):  TypedDocumentNode<InputType<GraphQLTypes["Subscription"], Z>, {}> {
+  return gql(Zeus("subscription", subscription));
+}
+`);
   });
 });
