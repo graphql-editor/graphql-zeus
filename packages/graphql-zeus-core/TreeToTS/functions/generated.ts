2edaba9cc8561292a90e320cf0b0389ad49a8cc3
aexol <aexol@aexol.com>
2023-01-13 14:57:16 +0100

    :sparkles: resolver for return type capture

--- packages/graphql-zeus-core/TreeToTS/functions/generated.ts
+++ packages/graphql-zeus-core/TreeToTS/functions/generated.ts
@@ -609,14 +609,19 @@ export const InternalArgsBuilt = ({
   return arb;
 };
 
-export const resolverFor = <X, T extends keyof ResolverInputTypes, Z extends keyof ResolverInputTypes[T]>(
+export const resolverFor = <
+  X,
+  T extends keyof ResolverInputTypes,
+  Z extends keyof ResolverInputTypes[T],
+  RET = unknown,
+>(
   type: T,
   field: Z,
   fn: (
     args: Required<ResolverInputTypes[T]>[Z] extends [infer Input, any] ? Input : any,
     source: any,
-  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : any,
-) => fn as (args?: any, source?: any) => any;
+  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : RET,
+) => fn as (args?: any, source?: any) => RET;
 
 export type UnwrapPromise<T> = T extends Promise<infer R> ? R : T;
 export type ZeusState<T extends (...args: any[]) => Promise<any>> = NonNullable<UnwrapPromise<ReturnType<T>>>;
--- packages/graphql-zeus-core/TreeToTS/functions/new/resolverFor.ts
+++ packages/graphql-zeus-core/TreeToTS/functions/new/resolverFor.ts
@@ -1,10 +1,15 @@
 import { ModelTypes, ResolverInputTypes } from '@/TreeToTS/functions/new/mocks';
 
-export const resolverFor = <X, T extends keyof ResolverInputTypes, Z extends keyof ResolverInputTypes[T]>(
+export const resolverFor = <
+  X,
+  T extends keyof ResolverInputTypes,
+  Z extends keyof ResolverInputTypes[T],
+  RET = unknown,
+>(
   type: T,
   field: Z,
   fn: (
     args: Required<ResolverInputTypes[T]>[Z] extends [infer Input, any] ? Input : any,
     source: any,
-  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : any,
-) => fn as (args?: any, source?: any) => any;
+  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : RET,
+) => fn as (args?: any, source?: any) => RET;
