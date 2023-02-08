c5c754d2c95c0ce16e2ac7fa9e935b45bfa9aade
aexol <aexol@aexol.com>
2023-02-08 22:40:55 +0100

    :bug: fixed resolverFor return type

--- packages/graphql-zeus-core/TreeToTS/functions/generated.ts
+++ packages/graphql-zeus-core/TreeToTS/functions/generated.ts
@@ -609,19 +609,14 @@ export const InternalArgsBuilt = ({
   return arb;
 };
 
-export const resolverFor = <
-  X,
-  T extends keyof ResolverInputTypes,
-  Z extends keyof ResolverInputTypes[T],
-  RET = unknown,
->(
+export const resolverFor = <X, T extends keyof ResolverInputTypes, Z extends keyof ResolverInputTypes[T]>(
   type: T,
   field: Z,
   fn: (
     args: Required<ResolverInputTypes[T]>[Z] extends [infer Input, any] ? Input : any,
     source: any,
-  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : RET,
-) => fn as (args?: any, source?: any) => RET;
+  ) => Z extends keyof ModelTypes[T] ? ModelTypes[T][Z] | Promise<ModelTypes[T][Z]> | X : never,
+) => fn as (args?: any, source?: any) => ReturnType<typeof fn>;
 
 export type UnwrapPromise<T> = T extends Promise<infer R> ? R : T;
 export type ZeusState<T extends (...args: any[]) => Promise<any>> = NonNullable<UnwrapPromise<ReturnType<T>>>;
